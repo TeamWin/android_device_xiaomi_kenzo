b0420f590bd3e2459ad7d3dc95b5e4edca6535db
LokManSiu <bjsiu0617@gmail.com>
2018-02-18 12:59:31 +0800

    Update Android.mk

--- init/Android.mk
+++ init/Android.mk
@@ -17,11 +17,12 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := \
     system/core/base/include \
     system/core/init
-LOCAL_CFLAGS := -Wall
 LOCAL_SRC_FILES := init_kenzo.cpp
 LOCAL_MODULE := libinit_kenzo
+
 include $(BUILD_STATIC_LIBRARY)
